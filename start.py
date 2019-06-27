f7b90e1977c432a0fe0b778354cfda36349f2c82
Aditya Gupta <addiegupta@gmail.com>
2019-06-27 12:19:16 +0530

    Parameterise python scripts Usage "python start.py 700" to start 700 calls Providing no integer will default to using 100 calls

--- start.py
+++ start.py
@@ -1,8 +1,12 @@
 import requests
+import sys
 url = 'http://127.0.0.2:8080/start-call/'
 
-callnum = 2500
-for i in range(1,callnum + 1):
+callnum = 100
+if(len(sys.argv)==2):
+    callnum = sys.argv[1]
+ 
+for i in range(1,int(callnum) + 1):
     response = requests.get(url + str(i))
     #print(response.text)
 print(str(callnum) +" calls started")
