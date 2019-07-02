ffa6c7fd3852921b94a5c26beb08ae2ef21089aa
Aditya Gupta <addiegupta@gmail.com>
2019-07-02 12:06:18 +0530

    Implement usage of HikariCP for connection pooling - Fix overload on database causing absence of updation of records - Contains lots of commented code of Hikari implementations, will be removed in next commit

--- src/main/scala/com/server/provision/PlanDbActor.scala
+++ src/main/scala/com/server/provision/PlanDbActor.scala
@@ -1,15 +1,12 @@
 
 package com.server.provision
 
-import akka.pattern.pipe
 import akka.actor.{Actor, ActorLogging, ActorSystem, Props}
 import akka.stream.ActorMaterializer
-import akka.stream.alpakka.slick.javadsl.SlickSession
 import com.server.provision.MediatorActor._
-import slick.basic.DatabaseConfig
-import slick.jdbc.JdbcProfile
-
-import scala.concurrent.Future
+import slick.jdbc.PostgresProfile.api._
+import scala.concurrent.duration.Duration
+import scala.concurrent.{Await, Future}
 
 object PlanDbActor{
     def props(implicit materializer: ActorMaterializer, system : ActorSystem) =
@@ -26,12 +23,19 @@ class PlanDbActor(implicit materializer: ActorMaterializer, system : ActorSystem
     with ActorLogging {
 
 
+    val db = Database.forConfig("sample")
+
     import com.server.provision.PlanDbActor._
 
-    val databaseConfig = DatabaseConfig.forConfig[JdbcProfile]("slick-postgres")
-    implicit val sess = SlickSession.forConfig(databaseConfig)
+//    val newPostgresDb = Database.forConfig("custom.postgres")
 
-    import sess.profile.api._
+//        val databaseConfig = DatabaseConfig.forConfig[PostgresProfile]("slick-postgres")
+//    val db = Database.forConfig("slick-postgres")
+//    val db = HikariCPJdbcDataSource.forConfig("slick-postgres",)
+
+    //    implicit val sess = SlickSession.forConfig(databaseConfig)
+
+    //    import sess.profile.api._
     implicit val ec = system.dispatcher
     //  val medActor: ActorRef = context.actorOf(Med.props, "medActor")
     class Plans(tag: Tag) extends Table[(Int,String, String, Int)](tag, "plans") {
@@ -47,15 +51,27 @@ class PlanDbActor(implicit materializer: ActorMaterializer, system : ActorSystem
 
         case FindPlanById(id:Int)=>
             log.info(s"FindPlanById called for id: $id")
-
+/*
             val action = plans.filter(_.id === id).map(u => (u.data_balance)).result.map(_.headOption.map {
                 case (data_balance) => data_balance
             })
-            val f: Future[Option[Int]] = sess.db.run(action)
+//            val f: Future[Option[Int]] = databaseConfig.db.run(action)
+            val f: Future[Option[Int]] = newPostgresDb.run(action)
+//            val f: Future[Option[Int]] = db.run(action)
 
 //            f.pipeTo(sender())
+            */
 
-            sender()! ReplyToMeter(f,id)
+            val action = plans.filter(_.id===id).map(u => (u.data_balance)).result.map(_.headOption.map{
+                case data_balance => data_balance
+            })
+
+            val queryResult = db.run(action)
+
+//            Await.result(queryResult,Duration.apply(5,"s")).foreach(println)
+
+            sender()! ReplyToMeter(queryResult,id)
+
 
         //        f.onComplete {
         //          case s => println(s"Result: $s")
@@ -71,6 +87,7 @@ class PlanDbActor(implicit materializer: ActorMaterializer, system : ActorSystem
             // Get the statement without having to specify an updated value:
             //    val sql = query.updateStatement
 
-            sess.db.run(updateAction)
+            db.run(updateAction)
+//            sess.db.run(updateAction)
     }
 }
