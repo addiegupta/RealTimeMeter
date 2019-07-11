368b0b0d3737c51d0987e0a3048949fd7e4dd584
Aditya Gupta <addiegupta@gmail.com>
2019-07-11 17:29:37 +0530

    Integrate akka-viz 0.1.2 with realtime meter - Replace akka http with spray for viz to work - Remove unused dependencies that were causing viz to not work - Change web interface to localhost from 127.0.0.2

--- build.sbt
+++ build.sbt
@@ -1,7 +1,7 @@
 name := "RealTimeMeter"
 
 version := "0.1"
-scalaVersion := "2.12.6"
+scalaVersion := "2.11.7"
 
 
 resolvers ++= Seq("Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/",
@@ -13,24 +13,16 @@ parallelExecution in Test := false
 fork := true
 
 libraryDependencies ++= {
-    val akkaVersion = "2.4.19"
+    val akkaVersion = "2.4.1"
     val akkaHttpVersion = "10.0.9"
     Seq(
         "com.typesafe.akka"       %%  "akka-actor"                     % akkaVersion,
-        "com.typesafe.akka"       %%  "akka-slf4j"                     % akkaVersion,
-        "com.typesafe.akka"       %%  "akka-stream"                    % akkaVersion,
-        "com.typesafe.akka"       %% "akka-http-core"                  % akkaHttpVersion,
-        "com.typesafe.akka"       %% "akka-http"                       % akkaHttpVersion,
-        "com.typesafe.akka"       %% "akka-http-spray-json"            % akkaHttpVersion,
-        "com.typesafe.akka"       %%  "akka-testkit"                   % akkaVersion   % "test",
-        "org.scalatest"           %% "scalatest"                       % "3.0.0"       % "test",
-        "com.github.nscala-time"  %% "nscala-time"                     % "2.22.0",
         "com.typesafe.slick"      %% "slick-hikaricp"                  % "3.3.0",
         "com.typesafe.slick"      %% "slick-codegen"                   % "3.3.0",
         "com.typesafe.slick"      %% "slick"                           % "3.3.0",
-        "org.scalatest"           %% "scalatest"                       % "3.0.5"        % "test",
         "org.postgresql"          %  "postgresql"                      % "42.2.6",
         "de.aktey.akka.visualmailbox" %% "collector"                   % "1.1.0",
-        "ch.qos.logback"          %  "logback-classic"                 % "1.2.3"
+        "io.spray" %% "spray-can" % "1.3.3",
+        "io.spray" %% "spray-routing" % "1.3.3"
     )
 }
--- src/main/scala/com/server/provision/WebServer.scala
+++ src/main/scala/com/server/provision/WebServer.scala
@@ -4,52 +4,47 @@ import java.util.concurrent.TimeUnit
 
 import akka.actor.{ActorRef, ActorSystem, Props}
 import akka.event.Logging
-import akka.http.scaladsl.Http
-import akka.http.scaladsl.Http.ServerBinding
-import akka.http.scaladsl.server.Directives._
-import akka.stream.ActorMaterializer
 import akka.util.Timeout
 import MediatorActor._
 import akka.routing.FromConfig
+import spray.routing.SimpleRoutingApp
 
 import scala.concurrent.duration._
 import scala.concurrent.{Await, ExecutionContext, Future}
 import scala.util.{Failure, Success}
 
-object WebServer {
+object WebServer extends SimpleRoutingApp {
 
   implicit val system = ActorSystem("RealTimeMeterSystem")
   implicit def executor: ExecutionContext = system.dispatcher
   protected val log = Logging(system.eventStream, "RealTimeMeter-log")
-  protected implicit val materializer: ActorMaterializer = ActorMaterializer()
 
   log.info("Starting planDbActor")
-//  val planDbActor: ActorRef = system.actorOf(PlanDbActor.props, "planDataActor")
-  val dbActorPool=system.actorOf(FromConfig.props(PlanDbActor.props),"random-router-pool-resizer")
+  val dbActorPool = system.actorOf(FromConfig.props(PlanDbActor.props), "random-router-pool-resizer")
 
 
   def main(args: Array[String]): Unit = {
 
-    val provisioningRoute =
-      get{
-        pathSingleSlash{
+    startServer(interface = "localhost", port = 8181) {
+      get {
+        pathSingleSlash {
           complete("RealTime Metering System is up and running!")
         } ~
-        path("start-call"){
-          parameters("id"){ id =>
-            try{
-              val mediatorActor = system.actorOf(MediatorActor.props(dbActorPool),s"mediator-$id")
-              log.info(s"Created mediator actor for id #$id# and now initiating meter")
-              mediatorActor ! InitiateMeter(Integer.valueOf(id))
-              complete(s" Starting call for id $id")
-            }catch {
-              case ex: akka.actor.InvalidActorNameException =>
-                log.info(s"Already in call for id #$id# , Exception caught $ex ")
-                complete(s"Cannot Start, Already in call for id $id")
+          path("start-call") {
+            parameters("id") { id =>
+              try {
+                val mediatorActor = system.actorOf(MediatorActor.props(dbActorPool), s"mediator-$id")
+                log.info(s"Created mediator actor for id #$id# and now initiating meter")
+                mediatorActor ! InitiateMeter(Integer.valueOf(id))
+                complete(s" Starting call for id $id")
+              } catch {
+                case ex: akka.actor.InvalidActorNameException =>
+                  log.info(s"Already in call for id #$id# , Exception caught $ex ")
+                  complete(s"Cannot Start, Already in call for id $id")
+              }
             }
-          }
-        } ~
-          path("stop-call" ){
+          } ~
+          path("stop-call") {
             parameters("id") { id =>
               val start = System.currentTimeMillis()
               implicit val timeout = Timeout(FiniteDuration(1, TimeUnit.SECONDS))
@@ -63,8 +58,8 @@ object WebServer {
                   complete(s"Unable to stop call for id: $id \nError: $ex")
               }
             }
-          }~
-          path("crash-meter-actor" ){
+          } ~
+          path("crash-meter-actor") {
             parameters("id") { id =>
               val start = System.currentTimeMillis()
               implicit val timeout = Timeout(FiniteDuration(1, TimeUnit.SECONDS))
@@ -80,28 +75,6 @@ object WebServer {
             }
           }
       }
-    val httpInterface = "127.0.0.2"
-    val httpPort = 8181
-
-    log.info(s"About to bind to: $httpInterface and: $httpPort")
-    val bindingFuture: Future[ServerBinding] = Http().bindAndHandle(provisioningRoute, httpInterface, httpPort)
-
-    bindingFuture.map { serverBinding =>
-      log.info(s"Bound to: ${serverBinding.localAddress} ")
-    }.onComplete {
-      case Success(value) => log.info("RealTimeMeter WebServer started successfully")
-      case Failure(ex) =>
-        log.error(ex, "Failed to bind to {}:{}!", httpInterface, httpPort)
-        Http().shutdownAllConnectionPools()
-        system.terminate()
-    }
-
-    scala.sys.addShutdownHook {
-      log.info("Terminating...")
-      Http().shutdownAllConnectionPools()
-      system.terminate()
-      Await.result(system.whenTerminated, 30.seconds)
-      log.info("Terminated... Bye")
     }
   }
 }
